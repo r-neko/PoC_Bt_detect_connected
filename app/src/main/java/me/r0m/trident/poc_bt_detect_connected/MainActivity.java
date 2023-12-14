9581b13d381c0797e2f0fb93692e90e1395db6ed
ろむねこ <nem@nem.x0.to>
2023-12-14 16:28:43 +0900

    実装

--- app/src/main/java/me/r0m/trident/poc_bt_detect_connected/MainActivity.java
+++ app/src/main/java/me/r0m/trident/poc_bt_detect_connected/MainActivity.java
@@ -1,12 +1,25 @@
 package me.r0m.trident.poc_bt_detect_connected;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +33,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        intentFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("BT", "No permission to connect bluetooth devices");
+            TextView textView = findViewById(R.id.btStatusTextView);
+            textView.setText("No permission to connect bluetooth devices");
+            return;
+        }
+        else {
+            Log.d("BT", "Permission to connect bluetooth devices granted");
+            TextView textView = findViewById(R.id.btStatusTextView);
+            textView.setText("Permission to connect bluetooth devices granted");
+        }
+
+        registerReceiver(receiver, intentFilter);
+
+
     }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction(); // may need to chain this to a recognizing function
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                Log.d("BT", "No permission to connect bluetooth devices");
+                return;
+            }
+            String deviceName = device.getName();
+            String deviceHardwareAddress = device.getAddress(); // MAC address
+
+            String deviceAddress = intent.getStringExtra(BluetoothDevice.EXTRA_DEVICE);
+            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                //Do something if connected
+                Log.d("BT", "Device connected");
+                ((TextView) findViewById(R.id.btConnectStatusTextView)).setText("Connected: \n\t" + deviceName + "\n\t" + deviceHardwareAddress);
+            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                //Do something if disconnected
+                Log.d("BT", "Device disconnected");
+            }
+        }
+    };
 }
